0b2a0bff7be5c408ad93796825bc117c1e6c11b3
RohanSk20 <9945045494k@gmail.com>
2026-03-02 06:56:04 +0530

    simple change

---
+++ main.py
@@ -0,0 +1,18 @@
+nums =list(map(int, input("enter nums: ").split()))
+dups_counter = 0
+missing_number  = 0
+print(len(nums))
+for i in range(len(nums)):
+    print(i)
+    if i == i+ 1 :
+        print("hello")
+        # dups_counter +=1
+        # missing_number = i+1
+        # print(missing_number)
+        # print(dups_counter)
+# final_array = []
+# final_array.append(dups_counter)
+# final_array.append(missing_number)
+# print(final_array)
+
+        
